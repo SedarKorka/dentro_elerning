44ba3b8226e3be5aac62028e2589958be61d009e
Mamadou Korka Dallo <53437647+SedarKorka@users.noreply.github.com>
2025-04-24 21:20:48 +0000

    change default url of backend

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import axios from 'axios'
 
 
 
-axios.defaults.baseURL = 'https://maxibas-59ed2d659bf7.herokuapp.com/api/v1/'
+axios.defaults.baseURL = 'https://maxibas.onrender.com/api/v1/'
 
 createApp(App).use(store).use(router, axios).mount('#app')
